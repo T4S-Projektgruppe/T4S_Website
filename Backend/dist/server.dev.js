d14090025fb9cc5d651917b1a152794b27432823
gotped17 <gotped17@htl-kaindorf.ac.at>
2020-06-20 13:01:22 +0200

    fixing minor things

---
+++ Backend/dist/server.dev.js
@@ -0,0 +1,54 @@
+"use strict";
+
+var express = require('express'),
+    path = require('path'),
+    app = express(),
+    fs = require('fs'),
+    compression = require('compression'),
+    api = require('./routes/api');
+
+var http = require('http');
+
+var https = require('https'); // important for https, will be added later
+
+/*const privateKey = fs.readFileSync('/etc/letsencrypt/live/thecrether.at/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/thecrether.at/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/thecrether.at/chain.pem', 'utf8');
+
+const credentials = {
+		key: privateKey,
+		cert: certificate,
+		ca: ca
+};
+*/
+
+/*app.use('/', function (req, res, next) {
+
+	console.log("not secure")
+	res.status(200).sendFile(path.join(process.cwd(), 'index.html'));
+
+	// Or, if you don't want to automatically detect the domain name from the request header, you can hard code it:
+	//        res.redirect('https://example.com' + req.url);
+})*/
+// all use things
+
+
+app.set('views', './views');
+app.use(api);
+app.use(compression());
+
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use('/static', express["static"](path.join(process.cwd(), '..', 'Frontend', 'static'))); // setting up http and https server
+
+var httpServer = http.createServer(app); //const httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(80, function () {
+  console.log('HTTP Server running on port 80');
+}); //httpsServer.listen(443, () => {
+//		console.log('HTTPS Server running on port 443');
+//});
